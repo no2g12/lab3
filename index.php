851d177f19ab2e173c9cbc50b58eaf4acbe2e9e3
no2g12 <no2g12@soton.ac.uk>
2013-10-17 15:17:41 +0100

    saved all files with php extension
    removed html files and added a functions.php file in a new folder called
    includes

--- index.php
+++ index.php
@@ -1,22 +1,14 @@
 <!DOCTYPE html>
+<?php include ("includes/functions.php"); ?>
 <html>
-	<head>
-		<title>Boyd's Used Motors</title>
+    <?php echo(head("Boyd's Used Motors")); ?>
 		
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 		<script src="../../assets/js/html5shiv.js"></script>
 		<script src="../../assets/js/respond.min.js"></script>
 		<![endif]-->
-		
-		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-		<link href="css/style.css" rel="stylesheet" media="screen">
-		
-		<script src="//code.jquery.com/jquery.js" type="text/javascript"></script>
-		<script src="js/bootstrap.min.js" type="text/javascript"></script>
-		<script src="js/script.js" type="text/javascript"></script>
-		
-	</head>
+
 	<body onLoad="startTimer()">
 		
 		<!-- container -->
